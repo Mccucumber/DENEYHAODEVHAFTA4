3840cc7a27615232b1ea8d757897001d924b1a17
Mccucumber <ufukberkbayraktar@gmail.com>
2021-06-11 22:04:48 +0300

    DENEYAPODEVHAFTA4
    Ufuk Berk Bayraktar

---
+++ deneyaphafta4odev/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+///  Ufuk Berk Bayraktar
+      ///KODLAYÝCÝ
+      /*
+int main()
+{   int sayi;
+    cout<<"sayi giriniz :"<<"\n"<<endl;
+    cin>>sayi;
+    if (sayi % 2 ==1){
+        if(sayi % 3==0){
+            cout<<"sayi 3 ile bolunur"<<endl;
+        }
+        else{
+            cout<<"sayi tektir ama 3 ile bolunmez"<<endl;
+        }
+    }
+    if (sayi % 2==0){
+      if(sayi % 4==0){
+            cout<<"sayi 4 ile kalansiz bolunmektedir"<<endl;
+        }
+        else{
+            cout<<"sayi cifttir ama 4 ile bölünmez"<<endl;
+    }
+    }
+    else{
+      cout<<"geçerli bir sayi giriniz"<<endl;
+    }
+    }
+*/
+          ///ANALÝZCÝ
+///cevap 6
+
+          ///TASARLAYÝCÝ
+ /* int main(){
+    int sayi;
+    cin>>sayi;
+        if (sayi>10){
+                sayi-=10;
+                cout<<sayi;
+                return 0;
+        }
+    sayi+=10;
+    cout<<sayi<<endl;
+    return 0;
+  }*/
+         ///
